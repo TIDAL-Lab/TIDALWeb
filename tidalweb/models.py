eb1f9bb37d6c5de1226420e1ed98160e6bc31a93
root <root@ip-172-31-11-180.us-west-2.compute.internal>
2014-03-17 19:03:05 +0000

    init commit py files

---
+++ tidalweb/models.py
@@ -0,0 +1,205 @@
+from django.db import models
+from django.core.urlresolvers import reverse
+from image_cropping import ImageRatioField
+
+
+class Publication(models.Model):
+    authors = models.CharField(max_length=255)
+    year = models.CharField(max_length = 255)
+    title = models.CharField(max_length=255)
+    journal = models.CharField(max_length = 255)
+    pages = models.CharField(max_length = 255, blank = True)
+    slug = models.SlugField(unique=True, max_length=255)
+    viewable = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    localCopy = models.FileField(upload_to = 'files/pubs/', blank = True)
+    weblink = models.URLField(blank = True)
+    pubType = models.CharField(max_length = 255, choices = [('journal', 'Journal Articles'), ('book', 'Book Chapters'), ('refConfs', 'Refereed Conference Papers'), ('presentations', 'Presentations and Posters'), ('workshops', 'Workshop Papers'), ('others', 'Other Papers')])
+    pubAffil = models.CharField(max_length = 255, choices = [('lab', 'TIDAL Lab'), ('personal', ('Personal'))])
+    class Meta:
+        ordering = ['-created']
+ 
+    def __unicode__(self):
+        return u'%s' % self.title
+ 
+#    def get_absolute_url(self):
+#        return reverse('viewsPub.pub', args=[self.slug])
+
+class Tag(models.Model):
+    word        = models.CharField(max_length=35)
+    slug        = models.CharField(max_length=250)
+    created  = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.word
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    active = models.BooleanField(default=True)
+    website = models.URLField(blank = True)
+    about = models.TextField()
+    thumb = models.ImageField(upload_to='images/projects/thumbs/')
+    cropping = ImageRatioField('thumb', '300x300')
+    slug = models.SlugField(unique=True, max_length=255)
+    viewable = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    tags = models.ManyToManyField(Tag)
+
+    def tagFilter(self):
+        return Tag.objects.all
+
+    def photoFilter(self):
+        return Photos.objects.filter(myproject=self)
+
+
+    def photoThumb(self):
+        return list(Photos.objects.filter(myproject=self))[:1]
+
+    def related(self):
+        listofProjects =[]
+        tags = self.tagFilter()
+        for project in Project.objects.all():
+            for tag in project.tagFilter():
+                for myTag in tags:
+                    if tag == myTag and project != self:
+                        listofProjects.append(project)
+
+        return listofProjects
+
+    class Meta:
+        ordering = ['-created']
+ 
+    def __unicode__(self):
+        return u'%s' % self.name
+ 
+    def get_absolute_url(self):
+        return reverse('tidalweb.views.project', args=[self.slug])
+
+
+class Photos(models.Model):
+    image = models.ImageField(upload_to='images/projects/')
+    myproject = models.ForeignKey(Project)
+
+class People(models.Model):
+    name = models.CharField(max_length=255)
+    netID = models.CharField(max_length = 255)
+    status = models.CharField(max_length = 255, choices = [('director', 'Director'), ('faculty', 'Faculty'), ('phd', 'PhD Student'), ('masters', 'Masters Student'), ('undergrad', 'Undergraduate Student')])
+    biography = models.TextField()
+    website = models.URLField()
+    linkedin = models.URLField()
+    photo = models.ImageField(upload_to = 'images/people/')
+    cropping = ImageRatioField('photo', '300x300')
+    cv = models.FileField(upload_to = 'files/cv/')
+    emailuser = models.CharField(max_length = 255)
+    emaildomain = models.CharField(max_length = 255)
+    slug = models.SlugField(unique=True, max_length=255)
+    viewable = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    projects = models.ManyToManyField(Project, blank = True)
+
+    publications = models.ManyToManyField(Publication, blank = True)
+
+ 
+    class Meta:
+        ordering = ['-created']
+ 
+    def __unicode__(self):
+        return u'%s' % self.name
+ 
+    def get_absolute_url(self):
+        return reverse('tidalweb.views.personal', args=[str(self.slug)])
+
+class rInterests(models.Model):
+    interests = models.CharField(max_length = 255)
+    people = models.ForeignKey(People)
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(People)
+    slug = models.SlugField(unique=True, max_length=255)
+    description = models.CharField(max_length=255)
+    content = models.TextField()
+    published = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+ 
+    class Meta:
+        ordering = ['-created']
+ 
+    def __unicode__(self):
+        return u'%s' % self.title
+ 
+    def get_absolute_url(self):
+        return reverse('tidalweb.views.post', args=[self.slug])
+
+class FrontImage(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to = 'images/frontpage/')
+    cropping = ImageRatioField('image', '940x500')
+    slug = models.SlugField(unique=True, max_length=255)
+    description = models.CharField(max_length=255, blank = True)
+    published = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+ 
+    class Meta:
+        ordering = ['-created']
+ 
+    def __unicode__(self):
+        return u'%s' % self.name
+ 
+    def get_absolute_url(self):
+        return reverse('tidalweb.views.images', args=[self.slug])
+
+class Partner(models.Model):
+    name = models.CharField(max_length=255)
+    institution = models.CharField(max_length=255)
+    affiliation = models.CharField(max_length = 255, choices = [('labs', 'Northwestern Labs'), ('depts','Northwestern Departments'), ('oustideLabs', 'Outside Labs'), ('orgs', 'External Organizations')])
+    logo = models.ImageField(upload_to = 'images/logos/', blank = True)
+    url = models.URLField()
+    slug = models.SlugField(unique=True, max_length=255)
+    viewable = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ['-created']
+ 
+    def __unicode__(self):
+        return u'%s' % self.name
+ 
+    def get_absolute_url(self):
+        return reverse('tidalweb.views.partner', args=[self.slug])
+
+class AffilPerson(models.Model):
+    name = models.CharField(max_length=255)
+    netID = models.CharField(max_length = 255)
+    status = models.CharField(max_length = 255, choices = [('director', 'Director'), ('faculty', 'Faculty'), ('phd', 'PhD Student'), ('masters', 'Masters Student'), ('undergrad', 'Undergraduate Student')])
+    website = models.URLField()
+    photo = models.ImageField(upload_to = 'images/people/', blank = True)
+    slug = models.SlugField(unique=True, max_length=255)
+    viewable = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+ 
+    def __unicode__(self):
+        return u'%s' % self.name
+ 
+    def get_absolute_url(self):
+        return reverse('tidalweb.views.personal', args=[self.slug])
+
+
+class FrontPageBlurb(models.Model):
+    title = models.CharField(max_length = 255)
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+
+
+
+
+
+
+
+
---
+++ tidalweb/urls.py
@@ -0,0 +1,26 @@
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.contrib import admin
+from django.contrib import staticfiles
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
+import views
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^index', views.index, name = 'index'),
+    url(r'^publications', views.pub, name = 'publications'),
+    url(r'^people/$', views.people, name = 'people'),
+    url(r'^portfolio/$', views.portfolio, name = 'portfolio'),
+    url(r'^news/$', views.news, name = 'news'),
+    url(r'^contact', views.contact, name = 'contact'),
+#   url(r'^$', 'blog.views.index'),
+#   url(r'^$', 'people.views.index'),
+    url(r'^news/(?P<slug>[^/]+)/$', views.post),
+    url(r'^people/(?P<slug>[\w\-]+)/$', views.personal),
+    url(r'^portfolio/(?P<slug>[\w\-]+)/$', views.project),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True }),
+
+) + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
