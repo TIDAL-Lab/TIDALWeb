236a6a8d2d56b8239471c23980af28360f703dba
Michael Horn <michael-horn@northwestern.edu>
2014-03-21 12:18:50 -0500

    Just one list of people with a global sort

--- tidalweb/views/viewsPeople.py
+++ tidalweb/views/viewsPeople.py
@@ -4,23 +4,24 @@ from tidalweb.models import People, AffilPerson
 from views import index
 
 def people(request):
-    # get the blog posts that are published
-	director = People.objects.filter(viewable=True, status = 'director')
-	faculty = People.objects.filter(viewable=True, status='faculty')
-	phd = People.objects.filter(viewable = True, status = 'phd')
-	masters = People.objects.filter(viewable = True, status = 'masters')
-	undergrad = People.objects.filter(viewable = True, status = 'undergrad')
+	peeps = People.objects.filter(viewable=True)
 
-	affilDirector = AffilPerson.objects.filter(viewable=True, status = 'director')
-	affilFaculty = AffilPerson.objects.filter(viewable=True, status='faculty')
-	affilPhd = AffilPerson.objects.filter(viewable = True, status = 'phd')
-	affilMasters = AffilPerson.objects.filter(viewable = True, status = 'masters')
-	affilUndergrad = AffilPerson.objects.filter(viewable = True, status = 'undergrad')
+	#director = People.objects.filter(viewable=True, status = 'director')
+	#faculty = People.objects.filter(viewable=True, status='faculty')
+	#phd = People.objects.filter(viewable = True, status = 'phd')
+	#masters = People.objects.filter(viewable = True, status = 'masters')
+	#undergrad = People.objects.filter(viewable = True, status = 'undergrad')
 
+	#affilDirector = AffilPerson.objects.filter(viewable=True, status = 'director')
+	#affilFaculty = AffilPerson.objects.filter(viewable=True, status='faculty')
+	#affilPhd = AffilPerson.objects.filter(viewable = True, status = 'phd')
+	#affilMasters = AffilPerson.objects.filter(viewable = True, status = 'masters')
+	#affilUndergrad = AffilPerson.objects.filter(viewable = True, status = 'undergrad')
 
-    # now return the rendered template
-	return render(request, 'people/people.html', {'director': director, 'faculty':faculty, 'phd':phd, 'masters':masters,'undergrad':undergrad, 'affilDirector':affilDirector, 'affilFaculty':affilFaculty, 'affilPhd':affilPhd, 'affilMasters':affilMasters, 'affilUndergrad':affilUndergrad})
+	#return render(request, 'people/people.html', {'director': director, 'faculty':faculty, 'phd':phd, 'masters':masters,'undergrad':undergrad, 'affilDirector':affilDirector, 'affilFaculty':affilFaculty, 'affilPhd':affilPhd, 'affilMasters':affilMasters, 'affilUndergrad':affilUndergrad})
+	return render(request, 'people/people.html', {'peeps': peeps})
 	
+
 def personal(request, slug):
 	# get the Post object
 	person = get_object_or_404(People, slug=slug)
