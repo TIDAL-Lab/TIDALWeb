ef3ea2dba6220e0e76a62d2a70613f5c93f22097
Drew Pollock <dhpollock@gmail.com>
2014-04-03 21:07:07 -0500

    updated margin-top for project object picture

--- tidalweb/settings.py
+++ tidalweb/settings.py
@@ -43,7 +43,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-#MEDIA_ROOT = os.path.join(PUBLIC_DIR, 'media')
+#MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
 MEDIA_ROOT = '/home/ec2-user/tidalweb/tidalweb/media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
