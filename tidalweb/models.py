16a42075a432f2eb46dcdae665822631554bd2d1
Michael Horn <michael-horn@northwestern.edu>
2015-04-22 22:07:33 -0500

    resized banner images

--- tidalweb/models.py
+++ tidalweb/models.py
@@ -148,7 +148,7 @@ class Post(models.Model):
 class FrontImage(models.Model):
     name = models.CharField(max_length=255)
     image = models.ImageField(upload_to = 'images/frontpage/')
-    cropping = ImageRatioField('image', '900x350')
+    cropping = ImageRatioField('image', '910x350')
     slug = models.SlugField(unique=True, max_length=255)
     description = models.CharField(max_length=255, blank = True)
     published = models.BooleanField(default=True)
