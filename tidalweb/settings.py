9360ef090d6c4fbd825c519b30bc477be71d2133
Michael Horn <michael-horn@northwestern.edu>
2015-06-02 16:16:02 -0500

    added static files configs to pull in bat2 static files

--- tidalweb/settings.py
+++ tidalweb/settings.py
@@ -41,32 +41,17 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-#MEDIA_ROOT = os.path.join(PUBLIC_DIR, 'media')
-MEDIA_ROOT = '/Users/msh801/github/TIDALWeb/tidalweb/public/media' #/home/ec2-user/tidalweb/tidalweb/media'
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = '/media/'
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+MEDIA_ROOT = os.path.join(PROJECT_DIR, 'public/media/')
 
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(PROJECT_DIR, '../static')
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    #os.path.join(PROJECT_DIR, 'static'),
+    os.path.join(PROJECT_DIR, '../../bat2/build'),
 )
 
 # List of finder classes that know how to find static files in
