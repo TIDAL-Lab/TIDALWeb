516d6a5dc8473d90924342618eae0d96e9554a94
Michael Horn <michael-horn@northwestern.edu>
2015-06-18 16:21:00 -0500

    adding meta data for social media buttons

--- tidalweb/views/blog.py
+++ tidalweb/views/blog.py
@@ -8,4 +8,7 @@ def bat(request):
   return render_to_response('blog/bat.html', { 'title' : 'Build-a-Tree Game' })
 
 def fishing(request):
-  return render_to_response('blog/fishing.html', { 'title' : 'Fishing with Friends' })
+  return render_to_response('blog/fishing.html', { 
+    'title' : 'TIDAL Lab | Fishing with Friends',
+    'author' : 'Michael Horn',
+    'description' : 'Fishing with Friends, a game about sustainable fishing.'})
