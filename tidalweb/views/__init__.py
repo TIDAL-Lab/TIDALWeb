6c649183ce8bf9231dbb59ac0d50baf0de70b687
Michael Horn <michael-horn@northwestern.edu>
2016-05-18 21:56:57 -0500

    reorganizing

--- tidalweb/views/__init__.py
+++
@@ -1,5 +0,0 @@
-from views import index, contact, greenhomegames, biomap, nettango, roberto, teaching, reese, frogpond, frogpondChallenge, news, pubs
-from viewsFront import images
-from viewsPartners import partner
-from viewsPeople import people, personal
-from viewsProject import portfolio, project
--- tidalweb/views/courses.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render, render_to_response
-
-def index(request):
-  return render_to_response('teaching.html')
- 
-def tidal(request):
-  return render_to_response('courses/tidal.html')
