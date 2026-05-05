8e8f2b7ccc551db2b3c417d61048c45e498f862e
Amartya <amartya916@gmail.com>
2026-05-04 22:52:42 -0500

    displays names for people in admin site

--- tidalweb/tidal/admin.py
+++ tidalweb/tidal/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from tidal.models import People, Post, Project, Publication, FrontImage
 
-admin.site.register(People)
-admin.site.register(Post)
+@admin.register(People)
+class PeopleAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_display_links = ('name',)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'slug', 'pubdate')
+    list_display_links = ('title',)
+
 admin.site.register(Project)
 admin.site.register(Publication)
 admin.site.register(FrontImage)
