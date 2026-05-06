13a41c490d99ecd48d8fe449acf536b12e623162
Amartya Banerjee <amartya916@gmail.com>
2026-05-06 16:32:49 -0500

    added --unfold-- forms for the users and groups

--- tidalweb/tidal/admin.py
+++ tidalweb/tidal/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.models import User, Group
+
 from unfold.admin import ModelAdmin
+from unfold.contrib.filters.admin import RangeDateFilter, RangeDateTimeFilter
+from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
+
 from tidal.models import People, Post, Project, Publication, FrontImage
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin, ModelAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    change_password_form = AdminPasswordChangeForm
+
+@admin.register(Group)
+class GroupAdmin(BaseGroupAdmin, ModelAdmin):
+    pass
 
 @admin.register(People)
 class PeopleAdmin(ModelAdmin):
@@ -11,6 +31,10 @@ class PeopleAdmin(ModelAdmin):
 class PostAdmin(ModelAdmin):
     list_display = ('title', 'author', 'slug', 'pubdate')
     list_display_links = ('title',)
+    list_filter = (
+            ("pubdate", RangeDateFilter),
+            ("created", RangeDateTimeFilter)
+    )
 
 @admin.register(Project)
 class ProjectAdmin(ModelAdmin):
